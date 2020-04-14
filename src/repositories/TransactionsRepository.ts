562f0bb6d7abd2201139f6ea282665466cd8807d
Gabriel Lemos <gabrielelias.lemos@gmail.com>
2020-04-14 16:03:32 -0300

    post and get transactions

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,23 +6,48 @@ interface Balance {
   total: number;
 }
 
+interface TransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
+  private balance: Balance;
+
   constructor() {
     this.transactions = [];
+    this.balance = { income: 0, outcome: 0, total: 0 };
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    return this.balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: TransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    const income = this.transactions
+      .filter(incomeTransaction => incomeTransaction.type === 'income')
+      .reduce((prevVal, elem) => prevVal + elem.value, 0);
+
+    const outcome = this.transactions
+      .filter(outcomeTransaction => outcomeTransaction.type === 'outcome')
+      .reduce((prevVal, elem) => prevVal + elem.value, 0);
+
+    const total = income - outcome;
+
+    this.balance = { income, outcome, total };
+
+    return transaction;
   }
 }
 
